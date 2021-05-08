605cccd5489f07902aea2c8604945c55214a75e4
Mevan Samaratunga <mevansam@gmail.com>
2021-05-07 21:59:07 -0400

    updated exports

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
 export * from './model/types';
+
+export * from './service/constants';
+export * from './service/action';
+export * from './service/state';
+export * from './service/user-space-service';
+
+export * from './service/providers/aws/provider';
