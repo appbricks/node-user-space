94e7f96cb376877b7921b59f64b5baaf067cfcfd
Mevan Samaratunga <mevansam@gmail.com>
2021-05-11 09:09:24 -0400

    updated exports

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,32 @@
 export * from './model/types';
 
 export * from './service/constants';
-export * from './service/action';
-export * from './service/state';
-export * from './service/user-space-service';
 
-export * from './service/providers/aws/provider';
+export {
+  USER_SEARCH,
+  GET_USER_DEVICES,
+  GET_DEVICE_ACCESS_REQUESTS,
+  ACTIVATE_USER_ON_DEVICE,
+  DELETE_USER_FROM_DEVICE,
+  DELETE_USER_FROM_SPACE,
+  DELETE_DEVICE,
+  GET_USER_DEVICE_TELEMETRY,
+  GET_USER_SPACES,
+  INVITE_USER_TO_SPACE,
+  REMOVE_USER_ACCESS_TO_SPACE,
+  DELETE_SPACE,
+  GET_USER_SPACE_TELEMETRY,
+  GET_SPACE_INVITATIONS,
+  ACCEPT_SPACE_INVITATION,
+  LEAVE_SPACE,
+  GET_USER_APPS,
+  GET_APP_INVITATIONS,
+  GET_USER_APP_TELEMETRY,
+} from './service/action';
+
+export { UserSpaceActionProps } from './service/action';
+export { UserSpaceStateProps} from './service/state';
+
+import UserSpaceService from './service/user-space-service';
+import AwsProvider from './service/providers/aws/provider'
+export { UserSpaceService, AwsProvider };
